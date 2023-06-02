1f7027e929cf68e0a2acf6ad04929d55200713ed
yarabramasta <yarabram111@gmail.com>
2023-06-02 12:58:38 +0700

    fix: tailwind not working

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./index.html', './src/**/*.{ts,js,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Wix Madefor Text"',
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          ...require('tailwindcss/defaultTheme').fontFamily.sans
+        ],
+        serif: [
+          '"Stardom"',
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          ...require('tailwindcss/defaultTheme').fontFamily.sans
+        ]
+      }
+    }
+  },
+  plugins: []
+};
+
+module.exports = config;
