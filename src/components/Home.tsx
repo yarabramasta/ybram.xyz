439e43aada143a9a0f4190914f098fb73190768e
yarabramasta <yarabram111@gmail.com>
2023-06-03 12:28:45 +0700

    fix: prev deployment errors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="safe-h-screen flex flex-col-reverse md:flex-row">
       <aside className="w-full border-t border-neutral-50/10 px-8 py-8 md:h-full  md:w-60 md:border-t-0 md:py-16">
         <ul className="flex h-fit w-full flex-row justify-between gap-4 md:h-full md:flex-col [&_li]:text-xs">
-          <MenuItem scene="about" max>
+          <MenuItem max>
             About +
           </MenuItem>
-          <MenuItem scene="contact">Contact +</MenuItem>
+          <MenuItem>Contact +</MenuItem>
         </ul>
       </aside>
       <div
